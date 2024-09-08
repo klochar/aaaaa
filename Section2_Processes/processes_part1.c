e14af5997efff8255934b4ec9bd7e1e0ac66378f
klochar <minayouhanna@outlook.com>
2024-09-08 16:27:02 -0400

    hierarchie figure 1 recree

--- Section2_Processes/processes_part1.c
+++ Section2_Processes/processes_part1.c
@@ -12,81 +12,69 @@
 void question1()
 {
 	//todo
-	// Create the root process (level 0)
-    pid_t level0 = getpid();
-    printf("Level 0, PID: %d\n", level0);
-    registerProc(level0, 0, 0, 0); // Root process registration
-
-    // Level 1 processes
-    pid_t level1_1, level1_2, level1_3, level1_4;
-
-    if ((level1_1 = fork()) == 0) {
-        // Level 2.1 process
-        pid_t level2_1;
-        printf("Level 1.1, PID: %d\n", getpid());
-        registerProc(getpid(), getppid(), 1, 1);
-
-        if ((level2_1 = fork()) == 0) {
-            printf("Level 2.1, PID: %d\n", getpid());
-            registerProc(getpid(), getppid(), 2, 1);
-        } else {
-            wait(NULL); // Wait for child to finish
-        }
-
-        exit(0); // Exit Level 1.1 process
-    } else if ((level1_2 = fork()) == 0) {
-        // Level 2.2 and 2.3 processes
-        pid_t level2_2, level2_3;
-        printf("Level 1.2, PID: %d\n", getpid());
-        registerProc(getpid(), getppid(), 1, 2);
-
-        if ((level2_2 = fork()) == 0) {
-            printf("Level 2.2, PID: %d\n", getpid());
-            registerProc(getpid(), getppid(), 2, 2);
-        } else if ((level2_3 = fork()) == 0) {
-            printf("Level 2.3, PID: %d\n", getpid());
-            registerProc(getpid(), getppid(), 2, 3);
-        } else {
-            wait(NULL); // Wait for children to finish
-        }
-
-        exit(0); // Exit Level 1.2 process
-    } else if ((level1_3 = fork()) == 0) {
-        // Level 2.4 process
-        pid_t level2_4;
-        printf("Level 1.3, PID: %d\n", getpid());
-        registerProc(getpid(), getppid(), 1, 3);
-
-        if ((level2_4 = fork()) == 0) {
-            printf("Level 2.4, PID: %d\n", getpid());
-            registerProc(getpid(), getppid(), 2, 4);
-        } else {
-            wait(NULL); // Wait for child to finish
-        }
-
-        exit(0); // Exit Level 1.3 process
-    } else if ((level1_4 = fork()) == 0) {
-        // Level 2.5, 2.6, and 2.7 processes
-        pid_t level2_5, level2_6, level2_7;
-        printf("Level 1.4, PID: %d\n", getpid());
-        registerProc(getpid(), getppid(), 1, 4);
-
-        if ((level2_5 = fork()) == 0) {
-            printf("Level 2.5, PID: %d\n", getpid());
-            registerProc(getpid(), getppid(), 2, 5);
-        } else if ((level2_6 = fork()) == 0) {
-            printf("Level 2.6, PID: %d\n", getpid());
-            registerProc(getpid(), getppid(), 2, 6);
-        } else if ((level2_7 = fork()) == 0) {
-            printf("Level 2.7, PID: %d\n", getpid());
-            registerProc(getpid(), getppid(), 2, 7);
-        } else {
-            wait(NULL); // Wait for children to finish
-        }
-
-        exit(0); // Exit Level 1.4 process
-    } else {
-        wait(NULL); // Wait for all Level 1 processes to finish
-    }
+	pid_t level0 = getpid();
+	printf("Level 0, PID: %d\n", level0);
+	//enfant
+	pid_t level1_1, level1_2, level1_3, level1_4;
+	if((level1_1=fork())==0){
+		//on est dans l enfant 1.1
+		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+		pid_t level2_1;
+		if((level2_1=fork())==0){
+			//on est enfant 2.1
+			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+			_exit(0);
+		}else {
+			wait(NULL);
+		}
+		_exit(0);
+	}else if((level1_2=fork())==0){
+		//on est dabs enfant 1.2
+		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+		pid_t level2_2, level2_3;
+		if((level2_2=fork())==0){
+			//on est dans enfant 2.2
+			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+                        _exit(0);
+		} else if((level2_3=fork())==0){
+			//on est dans enfant 2.3
+			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+			_exit(0);
+		} else {
+			while(wait(NULL)>0);
+		}
+		_exit(0);
+	}else if((level1_3=fork())==0){
+		//on est dabs enfant 1.3
+		pid_t level2_4;
+		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+		if((level2_4=fork())==0){
+			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+                        _exit(0);
+		} else {
+			wait(NULL);
+		}
+		_exit(0);
+	}else if((level1_4=fork())==0){
+		//on est dabs enfant 1.2
+		pid_t level2_5,level2_6,level2_7;
+		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+		if((level2_5=fork())==0){
+			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+                        _exit(0);
+		} else if((level2_6=fork())==0) {
+			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+                        _exit(0);
+		} else if((level2_7=fork())==0){
+			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
+                        _exit(0);
+		} else {
+			while(wait(NULL)>0);
+		}
+		_exit(0);
+	}else{
+		while(wait(NULL)>0);
+	}
+	
 }
 
