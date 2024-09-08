0bc575b20d7b6760d6bc4a303857b70fdb5c5cf9
klochar <minayouhanna@outlook.com>
2024-09-08 16:40:10 -0400

    added registerProc, mais pas sur du fonctionnement ni le resultat

--- Section2_Processes/processes_part1.c
+++ Section2_Processes/processes_part1.c
@@ -13,15 +13,19 @@ void question1()
 {
 	//todo
 	pid_t level0 = getpid();
+	//registerProc( pid_t pid, pid_t ppid, int procLevel, int procNum);
+	registerProc(level0, getppid(), 0, 0);
 	printf("Level 0, PID: %d\n", level0);
 	//enfant
 	pid_t level1_1, level1_2, level1_3, level1_4;
 	if((level1_1=fork())==0){
 		//on est dans l enfant 1.1
+		registerProc(level1_1, getppid(), 1, 1);
 		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
 		pid_t level2_1;
 		if((level2_1=fork())==0){
 			//on est enfant 2.1
+			registerProc(level2_1, getppid(), 2, 1);
 			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
 			_exit(0);
 		}else {
@@ -30,14 +34,17 @@ void question1()
 		_exit(0);
 	}else if((level1_2=fork())==0){
 		//on est dabs enfant 1.2
+		registerProc(level1_2, getppid(), 1, 2);
 		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
 		pid_t level2_2, level2_3;
 		if((level2_2=fork())==0){
 			//on est dans enfant 2.2
+			registerProc(level2_2, getppid(), 2, 2);
 			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
                         _exit(0);
 		} else if((level2_3=fork())==0){
 			//on est dans enfant 2.3
+			registerProc(level2_3, getppid(), 2, 3);
 			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
 			_exit(0);
 		} else {
@@ -46,9 +53,11 @@ void question1()
 		_exit(0);
 	}else if((level1_3=fork())==0){
 		//on est dabs enfant 1.3
+		registerProc(level1_3, getppid(), 1, 3);
 		pid_t level2_4;
 		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
 		if((level2_4=fork())==0){
+			registerProc(level2_4, getppid(), 2, 4);
 			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
                         _exit(0);
 		} else {
@@ -57,15 +66,19 @@ void question1()
 		_exit(0);
 	}else if((level1_4=fork())==0){
 		//on est dabs enfant 1.2
+		registerProc(level1_4, getppid(), 1, 4);
 		pid_t level2_5,level2_6,level2_7;
 		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
 		if((level2_5=fork())==0){
+			registerProc(level2_5, getppid(), 2, 5);
 			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
                         _exit(0);
 		} else if((level2_6=fork())==0) {
+			registerProc(level2_6, getppid(), 2, 6);
 			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
                         _exit(0);
 		} else if((level2_7=fork())==0){
+			registerProc(level2_7, getppid(), 2, 7);
 			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
                         _exit(0);
 		} else {
