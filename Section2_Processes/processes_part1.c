8936b680b98c7e83da1af125701d49a2d2e469e9
klochar <minayouhanna@outlook.com>
2024-09-21 10:19:10 -0400

    question 1

--- Section2_Processes/processes_part1.c
+++ Section2_Processes/processes_part1.c
@@ -8,86 +8,92 @@
 
 #include "libprocesslab/libprocesslab.h"
 
+int waitChildTotal() {
+    int status;
+    int nbCountChild = 0;
+    while (wait(&status) > 0) {
+        if (WIFEXITED(status)) {
+            nbCountChild += WEXITSTATUS(status);
+            nbCountChild ++;
+        }
+    }
+    return nbCountChild;
+}
 
-void question1()
-{
-	//todo
-	pid_t level0 = getpid();
-	//registerProc( pid_t pid, pid_t ppid, int procLevel, int procNum);
-	registerProc(level0, getppid(), 0, 0);
-	printf("Level 0, PID: %d\n", level0);
-	//enfant
-	pid_t level1_1, level1_2, level1_3, level1_4;
-	if((level1_1=fork())==0){
-		//on est dans l enfant 1.1
-		registerProc(level1_1, getppid(), 1, 1);
-		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-		pid_t level2_1;
-		if((level2_1=fork())==0){
-			//on est enfant 2.1
-			registerProc(level2_1, getppid(), 2, 1);
-			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-			_exit(0);
-		}else {
-			wait(NULL);
-		}
-		_exit(0);
-	}else if((level1_2=fork())==0){
-		//on est dabs enfant 1.2
-		registerProc(level1_2, getppid(), 1, 2);
-		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-		pid_t level2_2, level2_3;
-		if((level2_2=fork())==0){
-			//on est dans enfant 2.2
-			registerProc(level2_2, getppid(), 2, 2);
-			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-                        _exit(0);
-		} else if((level2_3=fork())==0){
-			//on est dans enfant 2.3
-			registerProc(level2_3, getppid(), 2, 3);
-			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-			_exit(0);
-		} else {
-			while(wait(NULL)>0);
-		}
-		_exit(0);
-	}else if((level1_3=fork())==0){
-		//on est dabs enfant 1.3
-		registerProc(level1_3, getppid(), 1, 3);
-		pid_t level2_4;
-		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-		if((level2_4=fork())==0){
-			registerProc(level2_4, getppid(), 2, 4);
-			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-                        _exit(0);
-		} else {
-			wait(NULL);
-		}
-		_exit(0);
-	}else if((level1_4=fork())==0){
-		//on est dabs enfant 1.2
-		registerProc(level1_4, getppid(), 1, 4);
-		pid_t level2_5,level2_6,level2_7;
-		printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-		if((level2_5=fork())==0){
-			registerProc(level2_5, getppid(), 2, 5);
-			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-                        _exit(0);
-		} else if((level2_6=fork())==0) {
-			registerProc(level2_6, getppid(), 2, 6);
-			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-                        _exit(0);
-		} else if((level2_7=fork())==0){
-			registerProc(level2_7, getppid(), 2, 7);
-			printf("je suis :%d et mon pere est :%d\n",getpid(),getppid());
-                        _exit(0);
-		} else {
-			while(wait(NULL)>0);
-		}
-		_exit(0);
-	}else{
-		while(wait(NULL)>0);
-	}
-	
+void question1() {
+
+    int nbCount = 0;
+    int status;
+
+	//proc de question1, main
+    registerProc(getpid(), getppid(), 0, 0);
+
+	//enfant1.1
+    if (fork() == 0) { 
+        registerProc(getpid(), getppid(), 1, 1);
+        int nbCount = 0;
+        if (fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 1);
+            _exit(0);
+        }
+        nbCount += waitChildTotal();
+        _exit(nbCount);
+    }
+    
+	//enfant 1.2
+    if (fork() == 0) {
+        registerProc(getpid(), getppid(), 1, 2);
+        int nbCount = 0;
+        if (fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 2);
+            _exit(0);
+        }
+        if (fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 3);
+            _exit(0);
+        }
+        nbCount += waitChildTotal();
+        _exit(nbCount);
+    }
+    
+	//enfant 1.3
+    if (fork() == 0) {
+        registerProc(getpid(), getppid(), 1, 3);
+        int nbCount = 0;
+        if (fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 4);
+            _exit(0);
+        }
+        nbCount += waitChildTotal();
+        _exit(nbCount);
+    }
+
+
+	//enfant 1.4
+    if (fork() == 0) {
+        registerProc(getpid(), getppid(), 1, 4);
+        int nbCount = 0;
+        if (fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 5);
+            _exit(0);
+        }
+        if (fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 6);
+            _exit(0);
+        }
+        if (fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 7);
+            _exit(0);
+        }
+        nbCount += waitChildTotal();
+        _exit(nbCount);
+    }
+
+    nbCount = waitChildTotal();
+    
+    //printf("nb enfant cree : %d", nbCount);
+    printProcRegistrations();
+    execlp("ls", "ls", "-l", NULL);
+    perror("execlp na pas marche");
 }
 
